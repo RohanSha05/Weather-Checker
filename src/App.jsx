791196c7c6da66a1d09e1606022371d3ccb3cab0
RohanSha05 <rhnshariar@gmail.com>
2024-09-14 20:20:09 +0600

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -26,37 +26,54 @@ const WeatherChecker = () => {
   };
 
   return (
-    <div className="flex flex-col items-center p-5 bg-blue-100 min-h-screen">
-      <h1 className="text-3xl font-bold mb-4  mt-20">Weather Checker</h1>
-      <input
-        className="border rounded p-2 mb-2 w-full max-w-xs"
-        type="text"
-        placeholder="Enter city"
-        value={city}
-        onChange={handleInputChange}
-      />
-      <button
-        className="bg-black text-white px-4 py-2 rounded"
-        onClick={getWeather}
-      >
-        Check Weather
-      </button>
+		<>
+			<div
+				className="hero min-h-screen"
+				style={{
+					backgroundImage:
+						"url(https://img.daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.webp)",
+				}}
+			>
+				<div className="hero-overlay bg-opacity-60 text-black"></div>
+				<div className="hero-content text-neutral-content text-center">
+					<div className="max-w-md">
+						<div className="flex flex-col items-center p-40 bg-blue-100 ">
+							<h1 className="text-3xl text-black font-bold mb-4 ">
+								Weather Checker
+							</h1>
+							<input
+								className="border rounded p-2 mb-2 w-full max-w-xs text-black"
+								type="text"
+								placeholder="Enter city"
+								value={city}
+								onChange={handleInputChange}
+							/>
+							<button
+								className="bg-black text-white px-4 py-2 rounded"
+								onClick={getWeather}
+							>
+								Check Weather
+							</button>
 
-      {error && <p className="text-red-500 mt-4">{error}</p>}
+							{error && <p className="text-red-500 mt-4">{error}</p>}
 
-      {weather && (
-        <div className="mt-4 bg-gray-300 shadow-md p-20 rounded text-center">
-          <h2 className="text-xl font-bold">
-            {weather.name}, {weather.sys.country}
-          </h2>
-          <p className="capitalize">{weather.weather[0].description}</p>
-          <p>Temperature: {weather.main.temp}°C</p>
-          <p>Humidity: {weather.main.humidity}%</p>
-          <p>Wind Speed: {weather.wind.speed} m/s</p>
-        </div>
-      )}
-    </div>
-  );
+							{weather && (
+								<div className="mt-4 bg-gray-300 text-black shadow-md p-20 rounded text-center">
+									<h2 className="text-xl font-bold">
+										{weather.name}, {weather.sys.country}
+									</h2>
+									<p className="capitalize">{weather.weather[0].description}</p>
+									<p>Temperature: {weather.main.temp}°C</p>
+									<p>Humidity: {weather.main.humidity}%</p>
+									<p>Wind Speed: {weather.wind.speed} m/s</p>
+								</div>
+							)}
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default WeatherChecker;
